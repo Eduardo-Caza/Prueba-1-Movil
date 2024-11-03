9db818012567d48be136a06589776de3b6807f1b
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-11-03 17:34:28 -0500

    App Mejorada Deber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  splash = true; // Propiedad splash para controlar la pantalla de presentación
+
+  constructor() {
+    // Ocultar la splash screen después de 3 segundos
+    setTimeout(() => {
+      this.splash = false;
+    }, 3000); // Cambia 3000 por el tiempo que desees en milisegundos
+  }
 }
